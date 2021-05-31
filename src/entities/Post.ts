388be3d1faca820e02fba02c5e1d9e00d65cc057
kirankumarrr <kirankumarrrreddy@gmail.com>
2021-05-31 23:53:48 +0530

    GraphQL Modules (Complete CRUD) GraphQL/CRUD/MikrORM

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,19 +1,26 @@
 //DB Table
 
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+
+@ObjectType()//Converted to object type entity
 @Entity() // this is db table
 export class Post {
 
+  @Field(()=>Int)
   @PrimaryKey() 
   id!: number;
-
+  
+  @Field(()=>String)
   @Property({type:'date'})//regular column
   createdAt: Date = new Date();
-
+  
+  @Field(()=>String)
   @Property({ type:'date', onUpdate: () => new Date() }) // hook to get new date
   updatedAt: Date = new Date();
-
+  
+  @Field()
   @Property({type:'text'})
   title!: string;
 
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import mikroOrm from "./mikro-orm.config";
+import express from "express";
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from "type-graphql";
+import { HelloReslover } from "./resolvers/hello";
+import { PostReslover } from "./resolvers/post";
+
 const main = async()=>{
     const orm = await MikroORM.init(mikroOrm)
     orm.getMigrator().up();//run migration when started automatically
-    //Creating POST 
-    // const post = orm.em.create(Post,{title:'my first post'})
-    // await orm.em.persistAndFlush(post)// posting now
-    // await orm.em.nativeInsert(Post,{title:'my first post'})// posting now
+    
+    const app =express()
 
-    //Fetching posts 
-    const posts = await orm.em.find(Post,{})
-    console.log("posts",posts)
+    app.get('/',(_,res)=>{
+        res.send('Hello')
+    })
+
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers :[HelloReslover, PostReslover],
+            validate : false
+        }),
+        context:()=>({ em:orm.em }) //passing orm for posting data into db
+    })
+
+    apolloServer.applyMiddleware({app})
+
+    app.listen(4000,()=>{
+        console.log("port started on 4000")
+    })
 }
 main().catch((error)=>{
     console.log(error)
